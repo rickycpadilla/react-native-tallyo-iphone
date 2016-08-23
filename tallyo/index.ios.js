dfe54b2e397f578a490963b1d9fcf044580abcbe
Ricky Padilla <rickycpadilla@gmail.com>
2016-08-23 08:34:03 -0600

    background and circle styled and dynamic.

--- tallyo/index.ios.js
+++ tallyo/index.ios.js
@@ -9,23 +9,30 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
 class tallyo extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
+        <Image
+          resizeMode="contain"
+          source={require('./images/Bg.png')}
+          style={{ flex:1, width: null, height: null, margin: 20}}
+        />
+        {/*<View style={styles.circle}></View>*/}
+        {/*<Text style={styles.welcome}>
           Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
+          </Text>
+          <Text style={styles.instructions}>
           To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
+          </Text>
+          <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
-        </Text>
+        </Text>*/}
       </View>
     );
   }
@@ -33,10 +40,11 @@ class tallyo extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flex: 3,
+    position: 'relative',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    backgroundColor: '#032529',
   },
   welcome: {
     fontSize: 20,
@@ -48,6 +56,13 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  circle: {
+    resizeMode: 'contain'
+    // height: 100,
+    // width: 100,
+    // borderRadius: 300,
+    // backgroundColor: '#31ACB6',
+  }
 });
 
 AppRegistry.registerComponent('tallyo', () => tallyo);
