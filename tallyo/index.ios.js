f9fb30a4fa424a56848a595f104fe2ac7a1f78f8
Ricky Padilla <rickycpadilla@gmail.com>
2016-08-23 11:56:35 -0600

    text added, navbar made white

--- tallyo/index.ios.js
+++ tallyo/index.ios.js
@@ -10,28 +10,37 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
+  Image,
+  StatusBar
 } from 'react-native';
 
 class tallyo extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <StatusBar
+          backgroundColor="blue"
+          barStyle="light-content"
+        />
+
         <Image
           resizeMode="contain"
           source={require('./images/Bg.png')}
-          style={{ flex:1, width: null, height: null, margin: 20}}
-        />
-        <Text style={styles.welcome}>
-          tallyo
-        </Text>
-        {/*<Text style={styles.instructions}>
-          To get started, edit index.ios.js
-          </Text>
-          <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>*/}
+          style={{ flex:1, width: null, height: null, margin: 20,
+            justifyContent: 'center',
+          alignItems: 'center',}}
+        >
+          <View style={styles.backdropView}>
+            <Text style={styles.num}>
+              0
+            </Text>
+            <Text style={styles.text}>
+              tallyo
+            </Text>
+
+          </View>
+        </Image>
+
       </View>
     );
   }
@@ -45,17 +54,32 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     backgroundColor: '#032529',
   },
-  welcome: {
-    fontSize: 20,
+  num: {
+    fontSize: 180,
     textAlign: 'center',
     margin: 10,
     position: 'relative',
     color: 'white'
   },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    position: 'relative',
+    color: 'white',
+    letterSpacing: 3,
+    opacity: 0.7
+  },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  backdropView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 200,
+    width: 200,
+    backgroundColor: 'rgba(0,0,0,0)',
   },
 
 });
